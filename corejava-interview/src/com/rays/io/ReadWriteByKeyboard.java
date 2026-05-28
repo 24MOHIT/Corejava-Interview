bf58c13ffba259454bdf2d04d2c9dcce19df448e
Mohit Prajapat <mohitprajapat242004@gmail.com>
2026-05-28 12:31:15 +0530

    28-05-2026

---
+++ corejava-interview/src/com/rays/io/ReadWriteByKeyboard.java
@@ -0,0 +1,31 @@
+package com.rays.io;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+
+public class ReadWriteByKeyboard {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		FileWriter fw = new FileWriter("D:\\io\\sandeep.txt");
+		
+		System.out.println("Enter Here.......");
+		
+		String str = reader.readLine();
+		
+		while(str != null) {
+			
+			fw.write(str);
+			
+			System.out.println(str);
+			
+			str = reader.readLine();
+		}
+		
+		reader.close();
+		fw.close();
+	}
+}
